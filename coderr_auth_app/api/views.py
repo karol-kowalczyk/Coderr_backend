4ed95d7182dde1e6c1892ba5413a46547462b19b
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-01-14 21:56:15 +0100

    implemented a loginView in the views.py in the auth api folder

--- coderr_auth_app/api/views.py
+++ coderr_auth_app/api/views.py
@@ -4,7 +4,9 @@ from rest_framework.views import APIView
 from coderr_app.models import User
 from .serializers import UserSerializer
 
+
 class UserRegistrationView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         serializer = UserSerializer(data=request.data)
 
@@ -12,3 +14,27 @@ class UserRegistrationView(APIView):
             user = serializer.save()
             return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(ObtainAuthToken):
+
+    permission_classes = [AllowAny]
+
+    def post(self, request, *arg, **kwarg):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({'detail': ['Falsche Anmeldedaten']}, status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(request=request, username=user.username, password=password)
+        if not user:
+            return Response({'detail': ['Falsche Anmeldedaten']}, status=status.HTTP_400_BAD_REQUEST)
+        data = {}
+        token, created = Token.objects.get_or_create(user=user)
+        data = {
+            'token': token.key,
+            'username': user.username,
+            'email': user.email,
+            'user_id': user.id
+        }
+        return Response(data, status=status.HTTP_200_OK)
