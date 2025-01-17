98ccf259cf429840dec636190573d0873854fc0e
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-01-17 21:13:30 +0100

    configured the first API Views for the UserProfileDataView and the OferrDetialViewset

--- coderr_app/api/permissions.py
+++ coderr_app/api/permissions.py
@@ -1,5 +1,19 @@
 from rest_framework import permissions
 
+class IsCustomerReadOnlyPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        if request.user.user_profile.type == 'customer':
+            return False
+            
+        return False
+
 class IsObjectOwnerOrAdminPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
@@ -42,4 +56,48 @@ class IsBusinessOrAdminPermission(permissions.BasePermission):
             return request.user.is_staff or request.user.user_profile.type == 'business'
         
         return False
+    
+
+class OrderAccessPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
         
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        if request.user.is_staff:
+            return request.method != 'POST'
+        
+        if request.user.user_profile.type == 'customer':
+                return request.method == 'POST'
+        
+        if request.user.user_profile.type == 'business':
+                return request.method == 'PATCH'
+        
+        return False
+    
+
+class IsReviewerOrAdminPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        
+        if request.user.is_staff:
+            return request.method != 'POST'
+
+        if request.method == 'POST':
+            return request.user.user_profile.type == 'customer'
+        
+        if request.method in ['PATCH', 'DELETE']:
+            return True
+        
+        return True
+    
+    def has_object_permission(self, request, view, obj):
+        if request.method in ['PATCH', 'DELETE']:
+            return obj.customer_user == request.user or request.user.is_staff
+        
+        return True
