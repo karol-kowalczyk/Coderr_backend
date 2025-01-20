35451f4655931d612a75fd5e21f2c9874ffad33c
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-01-20 21:16:14 +0100

    updated my admin.py file

--- coderr_app/admin.py
+++ coderr_app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import UserProfile, Offers, OfferDetails, Orders, Reviews
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ['user__username', 'location']
+    list_display = ['user__username', 'email', 'location']
+
+class OfferAdmin(admin.ModelAdmin):
+    list_filter = ['user__username', 'created_at']
+    list_display = ['title', 'description', 'user__username', 'created_at']
+
+class OfferDetailsAdmin(admin.ModelAdmin):
+    list_filter = ['offer', 'title']
+    list_display = ['title', 'offer', 'offer__user__username', 'price', 'offer_type']
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_filter = ['offer', 'title', 'customer_user', 'business_user', 'status']
+    list_display = ['title', 'offer', 'business_user', 'customer_user', 'status', 'offer_type']
+
+class ReviewsAdmin(admin.ModelAdmin):
+    list_filter = ['description', 'customer_user', 'business_user']
+    list_display = ['description', 'business_user', 'customer_user', 'rating']
+
+
+admin.site.register(UserProfile, UserAdmin)
+admin.site.register(Offers, OfferAdmin)
+admin.site.register(OfferDetails, OfferDetailsAdmin)
+admin.site.register(Orders, OrdersAdmin)
+admin.site.register(Reviews, ReviewsAdmin)
