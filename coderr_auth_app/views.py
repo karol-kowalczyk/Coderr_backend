e3af63c4c7abfa24bcbc99748097f53acf7eb4a3
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-01-20 20:29:04 +0100

    created an RegistrationView in the auth app

--- coderr_auth_app/views.py
+++ coderr_auth_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from .serializers import RegistrationSerializer
+from rest_framework import status
 
-# Create your views here.
+class RegistrationView(APIView):
+
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+
+        if serializer.is_valid():
+            saved_account = serializer.save()
+            token, created = Token.objects.get_or_create(user=saved_account)
+            data = {
+                'token': token.key,
+                'username': saved_account.username,
+                'email': saved_account.email,
+                'user_id': saved_account.pk
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
