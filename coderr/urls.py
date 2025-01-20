bd3e9f3f6a257ea94861836de8fe2bca8ea1d206
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-01-20 21:18:28 +0100

    urls bug is fixed

--- coderr/urls.py
+++ coderr/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from coderr_auth_app.api.views import UserRegistrationView
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from coderr_auth_app.api.views import RegistrationView, LoginView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('coderr_app.api.urls')),
+    path('api/login/', LoginView.as_view(), name='login'),
+    path('api/registration/', RegistrationView.as_view(), name='registration'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
