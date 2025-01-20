17da35fe096bacb0fb601fd9d53407fe4a15ed05
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-01-20 20:39:01 +0100

    fixed bug in my Registrationserializer

--- coderr_auth_app/api/views.py
+++ coderr_auth_app/api/views.py
@@ -1,22 +1,38 @@
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.views import APIView
-from coderr_app.models import User
-from .serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from .serializers import RegistrationSerializer
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework import status
 
+class RegistrationView(APIView):
+   
+    permission_classes = [AllowAny]
 
-class UserRegistrationView(APIView):
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
 
         if serializer.is_valid():
-            user = serializer.save()
-            return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+            saved_account = serializer.save()
+            token, created = Token.objects.get_or_create(user=saved_account)
+            data = {
+                'token': token.key,
+                'username': saved_account.username,
+                'email': saved_account.email,
+                'user_id': saved_account.pk
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+            
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class LoginView(ObtainAuthToken):
-
-
+   
+    permission_classes = [AllowAny]
 
     def post(self, request, *arg, **kwarg):
         username = request.data.get('username')
