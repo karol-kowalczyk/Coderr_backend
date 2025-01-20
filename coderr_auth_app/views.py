13666f6c2014bf3ed66ea2b73e010c435740c20a
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-01-20 20:30:51 +0100

    created an LoginView with user, token and a user.id

--- coderr_auth_app/views.py
+++ coderr_auth_app/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 from .serializers import RegistrationSerializer
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from rest_framework import status
 
 class RegistrationView(APIView):
@@ -25,3 +28,28 @@ class RegistrationView(APIView):
             return Response(data, status=status.HTTP_201_CREATED)
             
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(ObtainAuthToken):
+  
+    permission_classes = [AllowAny]
+
+    def post(self, request, *arg, **kwarg):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({'detail': ['Falsche Anmeldedaten']}, status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(request=request, username=user.username, password=password)
+        if not user:
+            return Response({'detail': ['Falsche Anmeldedaten']}, status=status.HTTP_400_BAD_REQUEST)
+        data = {}
+        token, created = Token.objects.get_or_create(user=user)
+        data = {
+            'token': token.key,
+            'username': user.username,
+            'email': user.email,
+            'user_id': user.id
+        }
+        return Response(data, status=status.HTTP_200_OK)
