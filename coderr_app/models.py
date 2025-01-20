274b8a0c8292dbe982cbf7783d55e78f398ca165
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-01-20 21:11:54 +0100

    added an offers detail model

--- coderr_app/models.py
+++ coderr_app/models.py
@@ -31,3 +31,36 @@ class Offers(models.Model):
     class Meta:
         ordering = ['title']
         verbose_name_plural = 'Offers'
+
+class OfferDetails(models.Model):
+    """
+    Details eines spezifischen Angebots, z. B. Preis und Typ.
+
+    Attributes:
+        offer (Offers): Referenz auf das zugehörige Angebot.
+        title (str): Titel des Angebotsdetails, max. 150 Zeichen.
+        revisions (int): Anzahl der zulässigen Überarbeitungen, Standard: -1 (unbegrenzt).
+        delivery_time_in_days (int): Lieferzeit in Tagen.
+        price (DecimalField): Preis für dieses Detailpaket.
+        features (JSONField): JSON-Daten für spezifische Merkmale oder Eigenschaften.
+        offer_type (str): Typ des Angebotsdetails, entweder 'basic', 'standard' oder 'premium'.
+    """
+    offer = models.ForeignKey(Offers, on_delete=models.CASCADE, related_name='details')
+    title = models.CharField(max_length=150)
+    revisions = models.IntegerField(default=-1)
+    delivery_time_in_days = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    features = models.JSONField()
+    OFFER_TYPES = [
+        ('basic', 'Basic'),
+        ('standard', 'Standard'),
+        ('premium', 'Premium')
+    ]
+    offer_type = models.CharField(max_length=50, choices=OFFER_TYPES)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+        verbose_name_plural = 'Offerdetails'
