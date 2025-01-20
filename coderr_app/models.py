23ad606e75059422fc76a53f09f8606d006ea177
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-01-20 21:11:00 +0100

    added an offers model

--- coderr_app/models.py
+++ coderr_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class User(models.Model):
     USER_TYPES = (
@@ -14,3 +15,19 @@ class User(models.Model):
     
     def __str__(self):
         return self.username
+
+class Offers(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='offers')
+    title = models.CharField(max_length=150)
+    image = models.FileField(upload_to='offers/', null=True, blank=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+        verbose_name_plural = 'Offers'
